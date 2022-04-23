e716135e2e3a42256d05cf34b48110a4fdf4721a
fmdavids <fmproade@gmail.com>
2022-04-23 15:42:47 +0100

    change todoapp to index.html

--- todoApp.js
+++ todoApp.js
@@ -1,10 +1,12 @@
-// let todoData = []
-let todoData = JSON.parse(localStorage.getItem('allTodoData'));
+var todoData = JSON.parse(localStorage.getItem('allTodoData'));
+if (todoData == null) {
+    todoData == [];
+}
 const dateDisplay = document.getElementById('dateDisplay');
-const render = (data) =>{
+const render = (data) => {
     let element = document.getElementById("content");
     element.innerHTML = "";
-    data.map((val, key)=>{
+    data.map((val, key) => {
         element.innerHTML += `
         <div class="card border-0 shadow-sm mb-3">
             <div class="card-body">
@@ -13,12 +15,10 @@ const render = (data) =>{
             <span class="float-end">${val.date}</span>
             </div>              
             <div class="float-end btn-btns pt-3">
-                <span class="badge  ${
+                <span class="badge  ${val.status == 'Pending' ? "bg-secondary" :
+                val.status == 'In-progress' ? "bg-warning" : "bg-success"
 
-                    val.status == 'Pending' ? "bg-secondary" :
-                    val.status == 'In-progress' ? "bg-warning" : "bg-success"
-
-                }">
+            }">
                     ${val.status}
                 </span>
                 <button class="btn" onclick="edit(${key})">
@@ -36,7 +36,7 @@ const render = (data) =>{
     });
 
     localStorage.setItem('allTodoData', JSON.stringify(data));
-    
+
 }
 render(todoData);
 
@@ -46,21 +46,21 @@ var editModal = new bootstrap.Modal(document.getElementById('editModal'))
 
 
 //create todo
-document.getElementById("todoForm").addEventListener('submit', (e) =>{
+document.getElementById("todoForm").addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     let todoDate = document.getElementById("todoDate").value;
     let todoItem = document.getElementById("addTask").value;
-    todoData.push({todoItem:todoItem, status:"Pending", date:todoDate});
+    todoData.push({ todoItem: todoItem, status: "Pending", date: todoDate });
     render(todoData);
     addModal.hide();
-    
+
 })
 
 
 //edit todo
 
-const edit = (id)=>{
+const edit = (id) => {
     editModal.show();
     document.getElementById("editTask").value = todoData[id].todoItem;
     document.getElementById("editStatus").value = todoData[id].status;
@@ -68,24 +68,24 @@ const edit = (id)=>{
 }
 
 //Save edit
-document.getElementById("editForm").addEventListener('submit', (e) =>{
+document.getElementById("editForm").addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     let todoItem = document.getElementById("editTask").value;
     let status = document.getElementById("editStatus").value;
     let id = document.getElementById("todoId").value;
-    
-    todoData[id].todoItem=todoItem;
-    todoData[id].status=status;
+
+    todoData[id].todoItem = todoItem;
+    todoData[id].status = status;
     render(todoData);
     editModal.hide();
-    
+
 })
 
 //trash
-const trash = (id) =>{
-    if(confirm("Are you sure you want to delete this?"))
-    todoData.splice(id, 1);
+const trash = (id) => {
+    if (confirm("Are you sure you want to delete this?"))
+        todoData.splice(id, 1);
     render(todoData);
 }
 
